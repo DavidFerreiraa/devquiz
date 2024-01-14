5422e768edb8bb3f9c46b60d6ddbc1a413c830f4
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-01-13 23:37:47 -0300

    feat: LevelButton widget added to the project

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +14,19 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: SizedBox(
+        height: 36,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          scrollDirection: Axis.horizontal,
+          children: [
+            LevelButtonWidget(label: "Fácil"),
+            LevelButtonWidget(label: "Médio"),
+            LevelButtonWidget(label: "Difícil"),
+            LevelButtonWidget(label: "Perito"),
+          ],
+        ),
+      )
     );
   }
 }
